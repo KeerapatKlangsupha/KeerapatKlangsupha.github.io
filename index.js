2ded2cff37cd74949f6483618d22297eac63f773
KeerapatKlangsupha <keerapat49@gmail.com>
2025-09-20 15:57:24 +0700

    teenoi

---
+++ index.js
@@ -0,0 +1,300 @@
+let price = 279;
+let total = [];
+
+// get equation
+const equationEle = document.getElementById("equation");
+function updateEquation(total) {
+    equationEle.textContent = total.join(" + ");
+}
+
+// เมื่อปุ่ม clicked ให้เพิ่มเลข ใน total
+document.getElementById("เนื้อสไลด์(เล็ก)").addEventListener('click', () => {
+    total.push(12);
+    updateEquation(total);
+});
+
+document.getElementById("เนื้อสไลด์(กลาง)").addEventListener('click', () => {
+    total.push(16);
+    updateEquation(total);
+});
+
+document.getElementById("เนื้อสไลด์(ใหญ่)").addEventListener('click', () => {
+    total.push(23);
+    updateEquation(total);
+});
+document.getElementById("สันคอหมูสไลด์(เล็ก)").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("สันคอหมูสไลด์(กลาง)").addEventListener('click', () => {
+    total.push(14);
+    updateEquation(total);
+});
+
+document.getElementById("สันคอหมูสไลด์(ใหญ่)").addEventListener('click', () => {
+    total.push(21);
+    updateEquation(total);
+});
+
+document.getElementById("สันนอกหมูสไลด์(เล็ก)").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("สันนอกหมูสไลด์(กลาง)").addEventListener('click', () => {
+    total.push(14);
+    updateEquation(total);
+});
+
+document.getElementById("สันนอกหมูสไลด์(ใหญ่)").addEventListener('click', () => {
+    total.push(22);
+    updateEquation(total);
+});
+
+document.getElementById("หมูสามชั้นสไลด์(เล็ก)").addEventListener('click', () => {
+    total.push(13);
+    updateEquation(total);
+});
+
+document.getElementById("หมูสามชั้นสไลด์(กลาง)").addEventListener('click', () => {
+    total.push(15);
+    updateEquation(total);
+});
+
+document.getElementById("หมูสามชั้นสไลด์(ใหญ่)").addEventListener('click', () => {
+    total.push(22);
+    updateEquation(total);
+});
+
+document.getElementById("ไก่สไลด์(เล็ก)").addEventListener('click', () => {
+    total.push(8);
+    updateEquation(total);
+});
+
+document.getElementById("ไก่สไลด์(กลาง)").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("ไก่สไลด์(ใหญ่)").addEventListener('click', () => {
+    total.push(17);
+    updateEquation(total);
+});
+
+document.getElementById("หมูลาวา").addEventListener('click', () => {
+    total.push(16);
+    updateEquation(total);
+});
+
+document.getElementById("ไก่ลาวา").addEventListener('click', () => {
+    total.push(13);
+    updateEquation(total);
+});
+
+document.getElementById("หมูพันสาหร่าย").addEventListener('click', () => {
+    total.push(10);
+    updateEquation(total);
+});
+
+
+document.getElementById("หมูไม้ไผ่").addEventListener('click', () => {
+    total.push(12);
+    updateEquation(total);
+});
+
+document.getElementById("ลูกชิ้นปลา").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("เต้าหู่ชีส").addEventListener('click', () => {
+    total.push(15);
+    updateEquation(total);
+});
+
+document.getElementById("ปูอัดชีส").addEventListener('click', () => {
+    total.push(10);
+    updateEquation(total);
+});
+
+document.getElementById("ปูอัด").addEventListener('click', () => {
+    total.push(7);
+    updateEquation(total);
+});
+
+document.getElementById("เต้าหู้ปลา").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("ปลาเส้น").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("เกี็ยวผักขมชีส").addEventListener('click', () => {
+    total.push(14);
+    updateEquation(total);
+});
+
+document.getElementById("ไข่ไก่").addEventListener('click', () => {
+    total.push(7);
+    updateEquation(total);
+});
+
+document.getElementById("เกี็ยวซ่าไก่").addEventListener('click', () => {
+    total.push(13);
+    updateEquation(total);
+});
+
+document.getElementById("ปลาม้วนไข่เค็ม").addEventListener('click', () => {
+    total.push(14);
+    updateEquation(total);
+});
+
+document.getElementById("ฟองเต้าหู่").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("เต้าหู่ไข่").addEventListener('click', () => {
+    total.push(6);
+    updateEquation(total);
+});
+document.getElementById("ปลาหมึก").addEventListener('click', () => {
+    total.push(16);
+    updateEquation(total);
+});
+
+document.getElementById("หอยแมลงภู่ชิลี่").addEventListener('click', () => {
+    total.push(18);
+    updateEquation(total);
+});
+
+document.getElementById("กุ้ง").addEventListener('click', () => {
+    total.push(20);
+    updateEquation(total);
+});
+
+document.getElementById("แมงกระพรุน").addEventListener('click', () => {
+    total.push(16);
+    updateEquation(total);
+});
+
+document.getElementById("ปลาหมึกกรอบ").addEventListener('click', () => {
+    total.push(18);
+    updateEquation(total);
+});
+
+document.getElementById("ปลาดอลลี่").addEventListener('click', () => {
+    total.push(14);
+    updateEquation(total);
+});
+
+document.getElementById("มาม่า").addEventListener('click', () => {
+    total.push(4);
+    updateEquation(total);
+});
+
+document.getElementById("บะหมี่หยก").addEventListener('click', () => {
+    total.push(5);
+    updateEquation(total);
+});
+
+document.getElementById("ชีส").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("เห็ดชิเมจิ").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("เห็ดออรินจิ").addEventListener('click', () => {
+    total.push(11);
+    updateEquation(total);
+});
+
+document.getElementById("เห็ดเข็มทอง").addEventListener('click', () => {
+    total.push(9);
+    updateEquation(total);
+});
+
+document.getElementById("ข้าวโพดหวาน").addEventListener('click', () => {
+    total.push(4);
+    updateEquation(total);
+});
+
+document.getElementById("แครอท").addEventListener('click', () => {
+    total.push(2);
+    updateEquation(total);
+});
+
+document.getElementById("กะหล่ำปลี").addEventListener('click', () => {
+    total.push(1);
+    updateEquation(total);
+});
+
+document.getElementById("สาหร่ายวากาเมะ").addEventListener('click', () => {
+    total.push(4);
+    updateEquation(total);
+});
+
+document.getElementById("ข้าวโพดอ่อน").addEventListener('click', () => {
+    total.push(5);
+    updateEquation(total);
+});
+
+document.getElementById("ผักรวม").addEventListener('click', () => {
+    total.push(8);
+    updateEquation(total);
+});
+
+document.getElementById("ผักกาดไต้หวัน").addEventListener('click', () => {
+    total.push(4);
+    updateEquation(total);
+});
+
+
+
+document.getElementById("ผักกาดขาว").addEventListener('click', () => {
+    total.push(3);
+    updateEquation(total);
+});
+
+
+
+
+//เมื่อกด addBtn จะรวมค่าใน total แล้วไปแสดงที่ result
+const resultEle = document.getElementById('result');
+const diffEle = document.getElementById('diff');
+document.getElementById("addBtn").addEventListener('click', function () {
+    let sum = 0;
+    for (let i = 0; i < total.length; i++) {
+        sum = sum + total[i];
+    }
+
+    resultEle.textContent = sum;
+    // diffEle.textContent = "ส่วนต่าง: " + (sum - price).toString();
+    // ถ้า ส่วนต่าง น้อยกว่า 0 ให้แสดงคำว่า ขาดทุน: xxx บาท
+    // ถ้า ส่วนต่าง มากกว่า 0 ให้แสดงคำว่า กำไร: xxx บาท
+    // else แสดง เท่าทุน !!
+    const diff = sum - price;
+    if (diff < 0) {
+        diffEle.textContent = `ขาดทุน: ${Math.abs(diff)} บาท`;
+    } else if (diff > 0) {
+        diffEle.textContent = `กำไร: ${Math.abs(diff)} บาท`;
+    } else {
+        diffEle.textContent = "เท่าทุน !!"
+    }
+});
+
+// เมื่อกด clearBtn จะล้างค่าใน total และ result
+document.getElementById("clearBtn").addEventListener('click', function () {
+    total = [];
+    updateEquation(total);
+    resultEle.textContent = "0";
+    diffEle.textContent = "";
+});
